8e3b1753024cfd17386a5447d093c262bcede2aa
stormcage <stormcage139@gmail.com>
2025-05-14 00:05:32 +0300

    Не успел сделать js

---
+++ FirstGear_landing/scripts/main.js
@@ -0,0 +1,13 @@
+// addEventListener
+
+// function write_to_lesson(){
+//     let main_form_containerElement = document.getElementById("forma")
+//     if (main_form_containerElement.style.display === "none") {
+//         main_form_containerElement.style.display = "flex";
+        
+//       } else {
+//         main_form_containerElement.style.display = "none";
+//       }
+//      main_form_containerElement.classList.toggle("show");
+    
+// }
