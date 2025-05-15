1b51b5a7d715f32a6a6f58c75ce5fe1317c11693
stormcage <stormcage139@gmail.com>
2025-05-15 22:40:05 +0300

    Last Version?

--- FirstGear_landing/scripts/main.js
+++ FirstGear_landing/scripts/main.js
@@ -68,33 +68,80 @@ giveFormResnponceBtnElement.addEventListener("click",(event) =>{
     
 })
 
-let menuOpenElement1 = document.getElementById("call-btn1")
-let menuOpenElement2 = document.getElementById("call-btn2")
+try{
+    let menuOpenElement1 = document.getElementById("call-btn1")
+    let menuOpenElement2 = document.getElementById("call-btn2")
+    menuOpenElement1.addEventListener("click",(e) =>{
+        if (main_form_containerElement.style.visibility === "" || main_form_containerElement.style.visibility === "hidden" ) {
+            console.log(main_form_containerElement.style.visibility)
+            main_form_containerElement.style.visibility = "visible";
+            main_form_containerElement.classList.add("shown");
+            document.body.style.overflow = "hidden"
+            menuOpenedStatus = true
+          } else {
+            menuOpenedStatus = false
+            console.log(main_form_containerElement.style.visibility)
+            main_form_containerElement.style.visibility = "hidden";
+            main_form_containerElement.classList.remove("shown");
+            document.body.style.overflow = ""
+          }
+    })
+    menuOpenElement2.addEventListener("click",(e) =>{
+        if (main_form_containerElement.style.visibility === "" || main_form_containerElement.style.visibility === "hidden" ) {
+            console.log(main_form_containerElement.style.visibility)
+            main_form_containerElement.style.visibility = "visible";
+            main_form_containerElement.classList.add("shown");
+            document.body.style.overflow = "hidden"
+            menuOpenedStatus = true
+          } else {
+            menuOpenedStatus = false
+            console.log(main_form_containerElement.style.visibility)
+            main_form_containerElement.style.visibility = "hidden";
+            main_form_containerElement.classList.remove("shown");
+            document.body.style.overflow = ""
+          }
+    })
+}
+catch (error){
+    console.log(error)
+}
 
-menuOpenElement1.addEventListener("click",(e) =>{
-    if (main_form_containerElement.style.visibility === "" || main_form_containerElement.style.visibility === "hidden" ) {
-        console.log(main_form_containerElement.style.visibility)
-        main_form_containerElement.style.visibility = "visible";
-        main_form_containerElement.classList.add("shown");
-        document.body.style.overflow = "hidden"
-        menuOpenedStatus = true
-      } else {
-        menuOpenedStatus = false
-        console.log(main_form_containerElement.style.visibility)
-        main_form_containerElement.style.visibility = "hidden";
-        main_form_containerElement.classList.remove("shown");
-        document.body.style.overflow = ""
-      }
-})
 
-// function write_to_lesson(){    
-//     if (main_form_containerElement.style.visibility === "" || main_form_containerElement.style.visibility === "hidden" ) {
-//         console.log(main_form_containerElement.style.visibility)
-//         main_form_containerElement.style.visibility = "visible";
-//         main_form_containerElement.classList.add("shown");
-//       } else {
-//         console.log(main_form_containerElement.style.visibility)
-//         main_form_containerElement.style.visibility = "hidden";
-//         main_form_containerElement.classList.remove("shown");
-//       }
-// }
+try{
+    let rofl_btn = document.getElementById("rofl-btn")
+    console.log(rofl_btn)
+
+    rofl_btn.addEventListener("click",(event) =>{
+        
+        event.preventDefault()
+        const inputPhoneElement = document.getElementById("form-file-phone-second")
+        const inputNameElement = document.getElementById("form-file-name-second")
+        let formSubmitStatus = document.getElementById("form-submit-succes-form-2")
+        
+        const phoneRegex = /^\+7\d{10}$/;
+        if (!phoneRegex.test(inputPhoneElement.value.trim())) {
+            formSubmitStatus.innerText = "Номер телефона должен быть в формате +7XXXXXXXXXX (всего 12 символов).";
+            formSubmitStatus.style.color = "red"
+            formSubmitStatus.style.display = "block";
+            return;
+        }
+        
+        if (inputNameElement.value.trim() === "") {
+            formSubmitStatus.style.color = "red"
+            formSubmitStatus.innerText = "Пожалуйста, введите имя.";
+            formSubmitStatus.style.display = "block";
+            return;
+        }
+        
+        else {
+            formSubmitStatus.innerText = "Вы успешно подали заявку!";
+            formSubmitStatus.style.color = "green"
+        }
+        console.log(formSubmitStatus.style.display )
+        formSubmitStatus.style.display = "block";
+        
+    })
+}
+catch (error){
+    console.log(error)
+}
